b550c307452530d91e5cc88f3e7f4a39d2dd6653
adityabiswas1408 <128647791+adityabiswas1408@users.noreply.github.com>
2024-11-21 23:54:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,29 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 
-# Initialize Flask app
 app = Flask(__name__)
-CORS(app)  # This enables CORS for all routes
+CORS(app)
 
-# Load the Excel data
-data = pd.read_excel("VOTE.xlsx")
+# Load the Excel file using an environment variable
+excel_file = os.getenv('EXCEL_FILE', 'VOTE.xlsx')
+data = pd.read_excel(excel_file)
 
 @app.route('/get-data', methods=['POST'])
 def get_data():
     try:
-        # Get the input data from the request
         user_input = request.json['query']
-        
-        # Convert both HRMS and IPAS columns to string for comparison
         data['HRMS'] = data['HRMS'].astype(str)
         data['IPAS'] = data['IPAS'].astype(str)
-        
-        # Search for the input in either 'HRMS' or 'IPAS' columns
+
         result = data[(data['HRMS'] == user_input) | (data['IPAS'] == user_input)]
-        
+
         if result.empty:
             return jsonify({"message": "No data found"}), 404
         return jsonify(result.to_dict(orient='records')), 200
     except Exception as e:
         return jsonify({"message": f"Error: {str(e)}"}), 500
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
